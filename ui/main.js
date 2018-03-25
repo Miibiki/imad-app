72152e806b54ac61b574e73335233696f4f2f77a
Miibiki <34130914+Miibiki@users.noreply.github.com>
2018-03-25 10:15:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
-var element=document.getElementById('madi');
+var image=document.getElementById('madi');
 img.onclick=function(){
   img.style.marginLeft='100px';
   
