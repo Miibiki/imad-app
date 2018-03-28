e27089313cd5edf2f5a335e4f43ea5c1c6fe5e91
Miibiki <34130914+Miibiki@users.noreply.github.com>
2018-03-28 12:59:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -168,7 +168,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/submit-name',function(req,res){
+app.get('/submit-name/:name',function(req,res){
    var name=req.params.name;
    names.push(name);
    res.send(JSON.stringify(names));
